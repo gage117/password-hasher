290648b7cc28ebca4b15187389d5f5d8fce33fd7
gage117 <gageeide@gmail.com>
2021-11-11 21:28:16 -0600

    Moved single password hashing logic to appropriate function

--- hashPassword.js
+++ hashPassword.js
@@ -9,26 +9,7 @@ if (!passwordInput) {
 // Copy an array of passwords here if you don't want to pass a stringified version as an argument
 let passwordArray = [];
 
-async function hashSinglePassword() {
-
-}
-
-async function hashPasswordArray() {
-  
-}
-
-async function main() {
-  // If string starts with a [ and ends with a ], try to parse as array
-  if (passwordInput.charAt(0) === "[" && passwordInput.charAt(passwordInput.length - 1) === "]") {
-    try {
-      passwordArray = JSON.parse(passwordInput);
-    } catch(err) {
-      console.log(err);
-      console.log("Attempt to parse input as array unsuccessful. Please check array formatting.")
-      return;
-    }
-  }
-
+async function hashSinglePassword(passwordStr) {
   const hashedPassword = await bcrypt.hash(process.argv[2], 12);
   
   const compareSuccess = await bcrypt.compare(process.argv[2], hashedPassword);
@@ -40,6 +21,27 @@ async function main() {
     console.log("Something went wrong, hashed password does not compare successfully with provided password");
     return;
   };
+}
+
+async function hashPasswordArray(passwordsArr) {
+
+}
+
+async function checkInput() {
+  // If string starts with a [ and ends with a ], try to parse as array
+  if (passwordInput.charAt(0) === "[" && passwordInput.charAt(passwordInput.length - 1) === "]") {
+    try {
+      passwordArray = JSON.parse(passwordInput);
+    } catch(err) {
+      console.log(err);
+      console.log("Attempt to parse input as array unsuccessful. Please check array formatting.")
+      return;
+    }
+  }
+
+  // If passwordArray has contents, hash those instead of passwordInput
+  //! 
+
 };
 
-main();
+checkInput();
